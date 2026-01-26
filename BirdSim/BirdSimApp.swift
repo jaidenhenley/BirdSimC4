a92a1188162e7976022dd507627e6233780fa07a
jaidenhenley <jaidenhenley@gmail.com>
2026-01-26 14:49:08 -0500

    Initial Commit

---
+++ BirdSim/BirdSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdSimApp.swift
+//  BirdSim
+//
+//  Created by Jaiden Henley on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct BirdSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
